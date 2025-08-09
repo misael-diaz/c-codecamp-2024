f0ef58517555f671791d23e122e63dfc2d9a9789
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-08-09 14:40:04 -0400

    gets user input
    NOTES
    stores the user input in a suitable placeholder (as big as a token)
    handles some possible input errors
    removes leading and trailing whitespace from the user input in order
    to compare against the problem answer (in a future commit); for
    this we are taking into account that the input is an ASCII string

--- src/golang/goquiz/main.c
+++ src/golang/goquiz/main.c
@@ -58,17 +58,51 @@ ssize_t tokenize(
 	return len;
 }
 
+ssize_t trim(
+	char ** const iter,
+	char ** const beg,
+	char ** const end
+)
+{
+	while ((**iter) && (' ' >= **iter)) {
+		++*iter;
+	}
+
+	if (!**iter) {
+		return -1;
+	}
+
+	*beg = *iter;
+	while ((**iter) && (' ' < **iter) && ('~' >= **iter)) {
+		++*iter;
+	}
+
+	if (!**iter) {
+		return -1;
+	}
+
+	**iter = 0;
+	*end = *iter;
+	return 0;
+}
+
 int main()
 {
 	char tokens[3][MAX_TOKEN_SIZE];
+	char word[MAX_TOKEN_SIZE];
 	errno = 0;
 	FILE *file = fopen("problems.csv", "r");
 	if (!file) {
 		fprintf(stderr, "main: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
+	char const *type = NULL;
+	char const *question = NULL;
+	char const *answer = NULL;
 	char *lineptr = NULL;
+	char *textptr = NULL;
 	size_t n = 0;
+	size_t m = 0;
 	int sw = 1;
 	do {
 		errno = 0;
@@ -89,7 +123,6 @@ int main()
 			char const *iter = lineptr;
 			char const *beg = NULL;
 			char const *end = NULL;
-			fprintf(stdout, "%s", lineptr);
 			if (-1 == parse(&iter, &beg, &end, ',')) {
 				goto err;
 			}
@@ -108,11 +141,41 @@ int main()
 			if (-1 == tokenize(tokens[2], beg, end, '\n')) {
 				goto err;
 			}
-			if (!strcmp("math", tokens[0])) {
-				long const res = atol(tokens[2]);
+			type = tokens[0];
+			question = tokens[1];
+			answer = tokens[2];
+			fprintf(stdout, "prompt: %s\n", question);
+			errno = 0;
+			if (-1 == getline(&textptr, &m, stdin)) {
+				fprintf(stderr, "main: %s\n", strerror(errno));
+				goto err;
+			} else {
+				fprintf(stdout, "input: %s", textptr);
+				if (MAX_TOKEN_SIZE < (1 + strlen(textptr))) {
+					fprintf(stderr, "main: %s\n", "TokenSizeError");
+					goto err;
+				}
+				char *iter = textptr;
+				char *beg = NULL;
+				char *end = NULL;
+				if (-1 == trim(&iter, &beg, &end)) {
+					fprintf(stderr, "main: %s\n", "UXInputError");
+					goto err;
+				}
+				memset(word, 0, MAX_TOKEN_SIZE);
+				ssize_t const bytes = (end - beg);
+				if (0 >= bytes) {
+					fprintf(stderr, "main: %s\n", "UXInputSizeError");
+					goto err;
+				}
+				memcpy(word, beg, bytes);
+				fprintf(stdout, "word: %s\n", word);
+			}
+			if (!strcmp("math", type)) {
+				long const res = atol(answer);
 				fprintf(stdout, "result: %ld\n", res);
-			} else if (!strcmp("text", tokens[0])) {
-				fprintf(stdout, "result: %s\n", tokens[2]);
+			} else if (!strcmp("text", type)) {
+				fprintf(stdout, "result: %s\n", answer);
 			} else {
 				fprintf(stderr, "main: %s\n", "ProblemTypeError");
 				goto err;
@@ -122,6 +185,11 @@ int main()
 	if (lineptr) {
 		free(lineptr);
 		lineptr = NULL;
+		n = 0;
+	}
+	if (textptr) {
+		free(textptr);
+		textptr = NULL;
 		n = 0;
 	}
 	fclose(file);
