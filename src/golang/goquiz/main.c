f2405f297038012ba552a10c1996b49b84846fef
misael-diaz <20134959+misael-diaz@users.noreply.github.com>
2025-08-09 08:53:26 -0400

    reads csv line by line
    NOTES
    aborts execution on error
    handles end-of-file EOF
    passed basic test
    adss problem set

--- src/golang/goquiz/main.c
+++ src/golang/goquiz/main.c
@@ -13,8 +13,28 @@ int main()
 	}
 	char *lineptr = NULL;
 	size_t n = 0;
-	int sw = 0;
+	int sw = 1;
 	do {
+		errno = 0;
+		ssize_t const bytes = getline(&lineptr, &n, file);
+		if (-1 == bytes) {
+			sw = 0;
+			if (errno) {
+				fprintf(stderr, "main: %s\n", strerror(errno));
+				if (lineptr) {
+					free(lineptr);
+					lineptr = NULL;
+					n = 0;
+				}
+				exit(EXIT_FAILURE);
+			}
+			fprintf(stdout, "%s\n", "EOF");
+		} else {
+			fprintf(stdout, "%s", lineptr);
+		}
 	} while (sw);
+	free(lineptr);
+	lineptr = NULL;
+	n = 0;
 	return 0;
 }
